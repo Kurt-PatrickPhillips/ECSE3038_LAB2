415560672bfe1aef1a1c10936b5911252f194631
Kurt-PatrickPhillips <Kurtpatrick.phillips@gmail.com>
2021-02-08 00:09:11 -0500

    commit

--- ServerMain.py
+++ ServerMain.py
@@ -1,4 +1,16 @@
 from flask import flask, request, jsonify
+import datetime
+from flask_cors import CORS
+
 app = Flask(__name__)
-if __name__== '__ServerMain__'
-app.run()
+CORS(app)
+dte = datetime.datetime.now()
+UserProfile = {
+    "sucess": True,
+    "data": {
+        "last_updated": "",
+        "username": "Hannibal",
+        "role": "Engineer",
+        "color": "yellow"
+    }
+}
